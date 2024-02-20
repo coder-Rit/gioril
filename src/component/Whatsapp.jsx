64c58d91c0d6180fad0a07f8ed6f6b7286bf16b7
coder-Rit <001patilritesh@gmail.com>
2024-02-20 17:37:41 +0530

    floting to rigth

--- src/component/Whatsapp.jsx
+++ src/component/Whatsapp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import img from '../assets/whatsapp.png'
 const WhatsAppIcon = () => {
   return (
-    <a href="https://wa.me/7003855041" target="_blank" rel="noopener noreferrer" className="fixed bottom-4 left-4 z-50">
+    <a href="https://wa.me/7003855041" target="_blank" rel="noopener noreferrer" className="fixed bottom-4 right-4 z-50">
       <img src={img} alt="WhatsApp Icon" className="w-12 h-12" />
     </a>
   );
