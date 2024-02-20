b851c8bf6d88ae7db86173917684335d99cf85a7
coder-Rit <001patilritesh@gmail.com>
2024-02-20 18:35:43 +0530

    caller btn

--- src/component/Whatsapp.jsx
+++ src/component/Whatsapp.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import img from '../assets/whatsapp.png'
+import PhoneIcon from "@mui/icons-material/Phone";
+
 const WhatsAppIcon = () => {
-  return (
-    <a href="https://wa.me/7003855041" target="_blank" rel="noopener noreferrer" className="fixed bottom-4 right-4 z-50">
+  return (<>
+  
+
+    <a href="https://wa.me/7003855041" target="_blank" rel="noopener noreferrer" className="fixed bottom-5 right-5 z-50">
       <img src={img} alt="WhatsApp Icon" className="w-12 h-12" />
     </a>
+    <a href="tel:+919508308432"  rel="noopener noreferrer" className="fixed bottom-5 left-5 z-50 rounded-full bg-white p-2">
+    <PhoneIcon></PhoneIcon>
+
+    </a>
+  </>
   );
 };
 
