838eecd85857598e43bdabf60de95da0f01ae931
coder-Rit <001patilritesh@gmail.com>
2024-02-20 20:13:38 +0530

    8961979319 change

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -32,19 +32,19 @@ const Footer = () => {
       </div>
 
       <div className="footerItem mx-auto text-center text-sm p-1 ">
-        <a href="https://wa.me/+919508308432" target="_blank">
+        <a href="https://wa.me/+8961979319" target="_blank">
           <span
             class="w-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            <BootstrapTooltip placement="top" title="+91 95083 08432">
+            <BootstrapTooltip placement="top" title="+918961979319">
               <WhatsAppIcon></WhatsAppIcon> Connect With WhatsApp
             </BootstrapTooltip>
           </span>
         </a>
 
-        <a href="tel:+919508308432">
+        <a href="tel:+8961979319">
           <span  class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">
-            <BootstrapTooltip placement="top" title="+91 95083 08432">
+            <BootstrapTooltip placement="top" title="+918961979319">
               <PhoneIcon></PhoneIcon> Call Us
             </BootstrapTooltip>
           </span>
