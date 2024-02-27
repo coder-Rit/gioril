9a3c616d5a37dc3a6cbf056b6e91bb1344809a63
coder-Rit <001patilritesh@gmail.com>
2024-02-27 19:20:39 +0530

    GoogleTagManagerScript updated

--- src/component/GoogleTagManagerScript.jsx
+++ src/component/GoogleTagManagerScript.jsx
@@ -2,26 +2,25 @@ import React, { useEffect } from 'react';
 
 const GoogleTagManagerScript = () => {
   useEffect(() => {
+    // Load Google Tag Manager script
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-11345970855';
+    script.async = true;
+    document.head.appendChild(script);
+
     // Initialize Google Tag Manager
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+    function gtag(){window.dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'AW-11345970855');
+
+    // Clean up function
+    return () => {
+      document.head.removeChild(script);
+    };
   }, []);
 
-  return (
-    <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11345970855"></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); }
-          gtag('js', new Date());
-          gtag('config', 'AW-11345970855');
-        `}
-      </script>
-    </>
-  );
+  return null; // or return any component you want to render
 };
 
 export default GoogleTagManagerScript;
