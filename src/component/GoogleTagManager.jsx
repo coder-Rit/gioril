e9631309e1ecdc461e01a1155139036839157b80
coder-Rit <001patilritesh@gmail.com>
2024-02-27 19:03:30 +0530

    ad sence update

--- src/component/GoogleTagManager.jsx
+++ src/component/GoogleTagManager.jsx
@@ -2,32 +2,25 @@ import React, { useEffect } from 'react';
 
 const GoogleTagManager = () => {
   useEffect(() => {
-    const initializeGoogleTagManager = () => {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){window.dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-11345970855');
-    };
-
-    if (!window.dataLayer) {
-      initializeGoogleTagManager();
-    } else {
-      // If dataLayer is already defined, just configure Google Tag Manager
-      window.dataLayer.push({ event: 'gtm.js', 'gtm.start': new Date().getTime() });
-    }
-
+    // Load Google Tag Manager script
     const script = document.createElement('script');
     script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-11345970855';
     script.async = true;
-
     document.head.appendChild(script);
 
+    // Initialize Google Tag Manager
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-11345970855');
+
+    // Clean up function
     return () => {
       document.head.removeChild(script);
     };
   }, []);
 
-  return null;
+  return null; // or return any component you want to render
 };
 
 export default GoogleTagManager;
